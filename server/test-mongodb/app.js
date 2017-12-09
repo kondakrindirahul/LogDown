48811369f90cef48596896e4f0a596b83c9c8922
RahulKondakrindi <rahul.kondakrindi@gmail.com>
2017-12-09 08:57:01 -0500

    Project Commit:1

--- server/test-mongodb/app.js
+++ server/test-mongodb/app.js
@@ -15,7 +15,7 @@ module.exports = function(app)
     var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
     var password = process.env.MLAB_PASSWORD_WEBDEV;
     connectionString = 'mongodb://' + username + ':' + password;
-    connectionString += '@ds163181.mlab.com:63181/webappmaker'; // user yours
+    connectionString += '@ds133816.mlab.com:33816/heroku_c9bsg11k'; // user yours
   }
 
 
